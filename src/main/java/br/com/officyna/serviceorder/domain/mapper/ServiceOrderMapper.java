6a8e0ad6c8a8b36d2e7781c824a3e277ecd79bd4
Vinicius Campanholi <viniciuscampanholi.vc@gmail.com>
2026-04-12 18:59:54 -0300

    feat(#45): implementa mapeamento de response para ordem de serviço com conversao de data no formato dd/MM/YYYY HH:mm

--- src/main/java/br/com/officyna/serviceorder/domain/mapper/ServiceOrderMapper.java
+++ src/main/java/br/com/officyna/serviceorder/domain/mapper/ServiceOrderMapper.java
@@ -4,24 +4,29 @@ import br.com.officyna.serviceorder.api.resources.ServiceOrderRequest;
 import br.com.officyna.serviceorder.api.resources.ServiceOrderResponse;
 import br.com.officyna.serviceorder.domain.LaborList;
 import br.com.officyna.serviceorder.domain.ServiceOrderEntity;
-import br.com.officyna.serviceorder.domain.ServiceOrderStatus;
 import br.com.officyna.serviceorder.domain.SupplyList;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
+import static br.com.officyna.serviceorder.domain.ServiceOrderStatus.RECEBIDA;
 
 @Component
 public class ServiceOrderMapper {
 
     public ServiceOrderEntity toEntity(ServiceOrderRequest request){
-        return ServiceOrderEntity.builder()
+        ServiceOrderEntity entity = ServiceOrderEntity.builder()
                 .vehicleId(request.getVehicleId())
                 .customerId(request.getCustomerId())
                 .informationText(request.getInformationText())
-                .laborsList(this.getLaborsList(request.getLaborsId()))
-                .supplyList(this.getSupplyLists(request.getSupplysId()))
-                .status(ServiceOrderStatus.RECEBIDA)
+                .laborsList(this.getLaborsList(request.getLaborIds()))
+                .supplyList(this.getSupplyLists(request.getSupplyIds()))
+                .status(RECEBIDA)
                 .build();
+        entity.setStatusDate(RECEBIDA);
+        return entity;
     }
 
     private List<LaborList> getLaborsList(List<String> laborsList){
@@ -40,6 +45,40 @@ public class ServiceOrderMapper {
 
     public ServiceOrderResponse toResponse(ServiceOrderEntity entity){
 
-        return null;
+        return ServiceOrderResponse.builder()
+                .serviceOrderId(entity.getId())
+                .serviceOrderNumber(entity.getServiceOrderNumber().toString())
+                .customerId(entity.getCustomerId())
+                .vehicleId(entity.getVehicleId())
+                .laborList(entity.getLaborsList())
+                .supplyList(entity.getSupplyList())
+                .informationText(entity.getInformationText())
+                .serviceOrderStatus(entity.getStatus().getStatusName())
+                .statusDate(this.getStatusDateByLastStatus(entity))
+                .totalBudgetAmount(String.format("R$ %.2f", entity.getTotalBudgetAmount()))
+                .createdAt(this.formatLocalDateTime(entity.getCreatedAt()))
+                .build();
+    }
+
+    private String getStatusDateByLastStatus(ServiceOrderEntity entity){
+        LocalDateTime statusDate = null;
+        switch (entity.getStatus()) {
+            case RECEBIDA -> statusDate = entity.getRegistrationDate();
+            case EM_DIAGNOSTICO -> statusDate = entity.getDiagnosisDate();
+            case AGUARDANDO_APROVACAO -> statusDate = entity.getClientSendDate();
+            case APROVADA -> statusDate = entity.getApprovalDate();
+            case EM_EXECUCAO -> statusDate = entity.getExecutionStartDate();
+            case FINALIZADA, RECUSADA -> statusDate = entity.getFinalizationDate();
+        }
+
+        return this.formatLocalDateTime(statusDate);
+    }
+
+    private String formatLocalDateTime(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return dateTime.format(formatter);
     }
 }
