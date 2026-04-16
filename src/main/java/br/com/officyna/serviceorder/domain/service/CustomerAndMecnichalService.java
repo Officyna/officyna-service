0da0f9feb91028fdc53b992371d86c4a47145041
Vinicius Campanholi <viniciuscampanholi.vc@gmail.com>
2026-04-16 16:17:03 -0300

    ref(#45): refatora metodos de busca de customers, users e labors para melhorar menutenibilidade do codigo

---
+++ src/main/java/br/com/officyna/serviceorder/domain/service/CustomerAndMecnichalService.java
@@ -0,0 +1,40 @@
+package br.com.officyna.serviceorder.domain.service;
+
+import br.com.officyna.administrative.customer.api.resources.CustomerResponse;
+import br.com.officyna.administrative.customer.domain.service.CustomerService;
+import br.com.officyna.administrative.user.api.resources.UserResponse;
+import br.com.officyna.administrative.user.domain.service.UserService;
+import br.com.officyna.serviceorder.domain.dto.CustomerDTO;
+import br.com.officyna.serviceorder.domain.dto.MechanicDTO;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomerAndMecnichalService {
+
+    private final UserService userService;
+
+    private final CustomerService customerService;
+
+    CustomerDTO getCustomer(@NotBlank(message = "ID do Cliente é obrigatório") String id) {
+        CustomerResponse response = customerService.findById(id);
+        return new CustomerDTO(response.id(),
+                response.name(),
+                response.phone(),
+                response.address().street(),
+                response.address().number(),
+                response.address().neighborhood(),
+                response.address().city(),
+                response.address().state(),
+                response.address().zipCode(),
+                response.address().complement());
+    }
+
+    MechanicDTO getMechanic(@NotBlank(message = "ID do Mecânico é obrigatório") String id) {
+        UserResponse response = userService.findById(id);
+        return new MechanicDTO(response.getId(), response.getName());
+    }
+}
