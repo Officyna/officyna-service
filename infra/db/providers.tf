40f199093852ff433d89a4cf396a5c1219cfefa5
Vinicius Campanholi <viniciuscampanholi.vc@gmail.com>
2026-08-18 21:16:34 -0300

    Revert "feat(#144): atualiza bucket"
    This reverts commit d5fd7bb4c29a46e94926058d5d5d1e0b8bd6d0a6.

--- infra/db/providers.tf
+++ infra/db/providers.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "officyna-terraform"
+    bucket = "officyna-terraform-state-995093551820-us-east-1-an"
     key    = "docdb/terraform.tfstate"
     region = "us-east-1"
   }
