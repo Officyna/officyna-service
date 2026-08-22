0b1a121410315be17985c68a84fc6b274f8fe171
Vinicius Campanholi <viniciuscampanholi.vc@gmail.com>
2026-08-22 16:36:52 -0300

    fix(#144): remove temporariamente a criação do banco de dados

--- infra/db/db.tf
+++ infra/db/db.tf
@@ -28,22 +28,22 @@ resource "aws_security_group" "docdb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# 3. Cria o Cluster do DocumentDB
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "officyna-mongodb-cluster"
-  engine                  = "docdb"
-  master_username         = var.db_username
-  master_password         = var.db_password
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids  = [aws_security_group.docdb_sg.id]
-}
-
-# 4. Cria a Instância do DocumentDB
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "officyna-mongodb-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
-}
+#
+# # 3. Cria o Cluster do DocumentDB
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "officyna-mongodb-cluster"
+#   engine                  = "docdb"
+#   master_username         = var.db_username
+#   master_password         = var.db_password
+#   skip_final_snapshot     = true
+#   db_subnet_group_name    = aws_docdb_subnet_group.default.name
+#   vpc_security_group_ids  = [aws_security_group.docdb_sg.id]
+# }
+#
+# # 4. Cria a Instância do DocumentDB
+# resource "aws_docdb_cluster_instance" "cluster_instances" {
+#   count              = 1
+#   identifier         = "officyna-mongodb-instance-${count.index}"
+#   cluster_identifier = aws_docdb_cluster.docdb.id
+#   instance_class     = "db.t3.medium"
+# }
--- infra/db/outputs.tf
+++ infra/db/outputs.tf
@@ -1,12 +1,12 @@
-output "docdb_endpoint" {
-  description = "Endpoint de conexao do cluster DocumentDB"
-  value       = aws_docdb_cluster.docdb.endpoint
-}
+# output "docdb_endpoint" {
+#   description = "Endpoint de conexao do cluster DocumentDB"
+#   value       = aws_docdb_cluster.docdb.endpoint
+# }
 
-output "docdb_port" {
-  description = "Porta do banco de dados"
-  value       = aws_docdb_cluster.docdb.port
-}
+# output "docdb_port" {
+#   description = "Porta do banco de dados"
+#   value       = aws_docdb_cluster.docdb.port
+# }
 
 output "vpc_cidr" {
   value = aws_vpc.vpc_fiap.cidr_block
